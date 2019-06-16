b3fbb6fbd9b564f5bbd511354d15cfbcde9d242c
Hao Wang <ge_nah@hotmail.com>
2019-06-16 15:59:20 -0700

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,16 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.length
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.songs.group(:artist_id).length
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    self.songs.map {|song| song.artist.name}
   end
 end
